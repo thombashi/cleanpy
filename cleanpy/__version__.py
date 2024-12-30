2c4753ac9b682c10628326e307534432264d4772
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2024-12-30 14:55:23 +0900

    Bump version to 0.5.1
    Signed-off-by: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>

--- cleanpy/__version__.py
+++ cleanpy/__version__.py
@@ -4,6 +4,6 @@ from typing import Final
 __author__: Final = "Tsuyoshi Hombashi"
 __copyright__: Final = f"Copyright 2020-2024, {__author__}"
 __license__: Final = "MIT License"
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 __maintainer__: Final = __author__
 __email__: Final = "tsuyoshi.hombashi@gmail.com"
